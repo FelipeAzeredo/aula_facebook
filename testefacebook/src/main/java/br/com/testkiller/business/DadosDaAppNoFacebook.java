7488c3d914ef84ea9c99c6d6a54b259d113fab64
Felipe Azeredo <limeirafelipe@gmail.com>
2019-07-18 21:10:14 -0300

    first commit

---
+++ testefacebook/src/main/java/br/com/testkiller/business/DadosDaAppNoFacebook.java
@@ -0,0 +1,7 @@
+package br.com.testkiller.business;
+public class DadosDaAppNoFacebook {
+
+	public static final String CLIENT_SECRET = "bf3beae82dd0129841efe8f484a435ff";
+	public static final String CLIENT_ID = "1252171992385223";
+	public static final String REDIRECT_URI = "http://localhost:8080/loginfbresponse";
+}
